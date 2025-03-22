192a1c2fb8b2984d69db693bc95ff068d8c6e20d
lanangido <idolanang@gmail.com>
2025-03-22 14:54:16 +0700

    Feat: auth verifikasi register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import * as Yup from "yup";
 
 type TRegister = {
 
@@ -9,11 +10,43 @@ type TRegister = {
     confirmPassword: string;
 }
 
+const registerValidateSchema = Yup.object({
+    fullName: Yup.string().required(),
+    username: Yup.string().required(),
+    email: Yup.string().email().required(),
+    password: Yup.string().required(),
+    confirmPassword: Yup.string().required().oneOf([Yup.ref("password"), ""], "Password harus sama"),
+})
+
 export default {
     async register(req: Request, res: Response) {
-
-        const {fullName, username, email, password, confirmPassword} = 
+        
+      const {fullName, username, email, password, confirmPassword} = 
         req.body as unknown as TRegister;
+        
+        try{ 
+            await registerValidateSchema.validate({
+            fullName,
+            username,
+            email,
+            password,
+            confirmPassword
+        });
+         res.status(200).json({
+            message: "Success Registration",
+            data: {
+                fullName, 
+                username,
+                email,
+            }
+         })
+        } catch (error){
+            const err = error as unknown as Error;
+            res.status(400).json({
+                message: err.message,
+                data:null,
+            });
+        }
         
     },
 }
